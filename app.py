a873b1c860fbb7999971342c6aea82e2f72bd658
Nogueira <swilliam000@hotmail.com>
2026-07-22 15:10:21 -0300

    Building a database

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from models.user import User
+from database import db
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "your_secret_key"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
+
+db.init_app(app)
+
+
+@app.route("/hello-world", methods=["GET"])
+def hello_world():
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
